838df9a70a712b3a3c6621df761f27631e840a1f
seungminng123 <ssmmkim1025@naver.com>
2025-12-11 16:28:41 +0900

    alice 버전이에여

--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,6 +3,7 @@ package mygit;
 public class C {
     public static void main(String[] args) {
         System.out.println("C: start program");
+        System.out.println("C: alice 수정");
         new A().printA();
         new B().printB();
     }
