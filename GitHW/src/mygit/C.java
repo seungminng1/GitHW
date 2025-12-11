81c241ce9e4f84be9f5addcbed9d13387b20fd49
seungminng123 <ssmmkim1025@naver.com>
2025-12-11 16:30:54 +0900

    마스터수정

--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -3,6 +3,7 @@ package mygit;
 public class C {
     public static void main(String[] args) {
         System.out.println("C: start program");
+        System.out.println("C: 마스터 수정함");
         new A().printA();
         new B().printB();
     }
